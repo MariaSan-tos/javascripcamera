9491e062009f7a3ed687a886e8932f887fdd0659
Maria Santos <130001344+MariaSan-tos@users.noreply.github.com>
2024-04-01 17:45:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 const cameraFeed = document.getElementById('video');
 
-setTimeout(() => {
-    document.querySelector('h1').innerHTML = "BOOOO!! PRIMEIRO DE ABRIL!!!";
-}, 2500);
-
-setTimeout(() => {
-    document.alert("VOCÊ É A PIADA!");
-}, 2800);
-
 async function startCamera() {
     try {
         const stream = await navigator.mediaDevices.getUserMedia({video: true});
@@ -19,3 +11,11 @@ async function startCamera() {
 }
 
 startCamera();
+
+setTimeout(() => {
+    document.querySelector('h1').innerHTML = "BOOOO!! PRIMEIRO DE ABRIL!!!";
+}, 2500);
+
+setTimeout(() => {
+    window.alert("VOCÊ É A PIADA!");
+}, 2800);
