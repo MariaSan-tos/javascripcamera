6f3514ac95fdb6386af6c0c2e5415e05038504d8
Maria Santos <130001344+MariaSan-tos@users.noreply.github.com>
2024-04-01 17:27:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const cameraFeed = document.getElementById('video');
 
+setTimeout(() => {
+    document.querySelector('h1').innerHTML = "BOOOO!! PRIMEIRO DE ABRIL!!!";
+}, 2500);
+
 async function startCamera() {
     try {
         const stream = await navigator.mediaDevices.getUserMedia({video: true});
         
-        video.srcObject = stream
+        video.srcObject = stream;
     } catch (error) {
         console.log("Error ao acessar a camera: ", error);
     }
 }
 
-startCamera()
+startCamera();
